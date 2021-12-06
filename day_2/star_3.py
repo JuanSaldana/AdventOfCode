80fde71ef758b7b43cfe4852d9ffbb6dcb0c1c57
Juan Saldana <jsaldana@valiot.io>
2021-12-05 18:25:51 -0600

    day-2 completed

---
+++ day_2/star_3.py
@@ -0,0 +1,54 @@
+﻿"""
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
+
+forward 5 adds 5 to your horizontal position, a total of 5.
+down 5 adds 5 to your depth, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13.
+up 3 decreases your depth by 3, resulting in a value of 2.
+down 8 adds 8 to your depth, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15.
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+Your puzzle answer was 1989265.
+"""
+
+
+def follow_instructions(initial_pos, instructions):
+    pos = initial_pos
+    for instruction in instructions:
+        command = instruction.split()[0]
+        value = int(instruction.split()[1])
+        if command == 'forward':
+            pos[0] += value
+        elif command == 'down':
+            pos[1] += value
+        elif command == 'up':
+            pos[1] -= value
+        else:
+            pos[0] -= value
+    return pos
+
+
+if __name__ == '__main__':
+    from INPUT import INPUT_DATA as instructions
+    final_pos = follow_instructions([0, 0], instructions)
+    print(final_pos[0]*final_pos[1])
